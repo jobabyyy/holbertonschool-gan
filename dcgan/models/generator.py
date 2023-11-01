fcf50276675d599f9395fbfd52cb1f973b50c19b
Johanna Avila <joeannaavila123@gmail.com>
2023-11-01 17:15:31 -0500

    started over and redid layers a bit

--- dcgan/models/generator.py
+++ dcgan/models/generator.py
@@ -4,78 +4,39 @@ Builds and
 compiles generator model."""
 
 
-"""import tensorflow as tf
-from tensorflow.keras.layers import Dense, BatchNormalization, LeakyReLU, Reshape"""
-
-
 import tensorflow as tf
-from tensorflow.keras.layers import Dense, Reshape, Conv2DTranspose
+from tensorflow import keras
 
-def build_generator(input_shape, latent_dim=100):
-    model = tf.keras.Sequential()
 
-    # Start with a fully connected layer to transform noise into a suitable tensor shape
-    model.add(Dense(7 * 7 * 128, input_shape=(latent_dim,)))
-    model.add(tf.keras.layers.BatchNormalization())
-    model.add(tf.keras.layers.LeakyReLU())
+def generator():
+    print("Initializing generator model...")
+    
+    model = keras.Sequential()
+    model.add(keras.layers.Dense(7*7*256, use_bias=False, input_shape=(100,)))
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.LeakyReLU())
 
-    # Reshape into a 7x7x128 tensor
-    model.add(Reshape((7, 7, 128)))
+    print("Reshaping output...")
+    model.add(keras.layers.Reshape((7, 7, 256)))
+    assert model.output_shape == (None, 7, 7, 256) 
 
-    # Upsample to 14x14x64 tensor
-    model.add(Conv2DTranspose(64, (5, 5), strides=(2, 2), padding='same'))
-    model.add(tf.keras.layers.BatchNormalization())
-    model.add(tf.keras.layers.LeakyReLU())
+    print("Adding first Conv2DTranspose layer...")
+    model.add(keras.layers.Conv2DTranspose(128, (5, 5), strides=(1, 1), padding='same', use_bias=False))
+    assert model.output_shape == (None, 7, 7, 128)
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.LeakyReLU())
 
-    # Upsample to 28x28x1 tensor
-    model.add(Conv2DTranspose(1, (5, 5), strides=(2, 2), padding='same', activation='tanh'))
+    print("Adding second Conv2DTranspose layer...")
+    model.add(keras.layers.Conv2DTranspose(64, (5, 5), strides=(2, 2), padding='same', use_bias=False))
+    assert model.output_shape == (None, 14, 14, 64)
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.LeakyReLU())
 
+    print("Adding final Conv2DTranspose layer...")
+    model.add(keras.layers.Conv2DTranspose(1, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
+    assert model.output_shape == (None, 28, 28, 1)
+    
+    print("Generator model constructed!")
     return model
 
-
-"""def build_generator(input_shape, latent_dim=100):
-    # Calculate the number of units needed for the initial Dense layer
-    num_units = 7 * 7 * 128
-
-    # Define the generator model
-    generator = tf.keras.Sequential()
-
-    # Add a fully connected layer with appropriate input shape
-    generator.add(Dense(num_units, input_shape=input_shape))
-    generator.add(LeakyReLU(alpha=0.2))
-    generator.add(BatchNormalization(momentum=0.8))
-
-    # Reshape to match the shape expected by convolutional layers
-    generator.add(Reshape((7, 7, 128)))
-
-    # Add convolutional layers with upsampling
-    generator.add(tf.keras.layers.Conv2DTranspose(128, (5, 5), strides=(2, 2), padding='same'))
-    generator.add(LeakyReLU(alpha=0.2))
-    generator.add(BatchNormalization(momentum=0.8))
-
-    generator.add(tf.keras.layers.Conv2DTranspose(64, (5, 5), strides=(2, 2), padding='same'))
-    generator.add(LeakyReLU(alpha=0.2))
-    generator.add(BatchNormalization(momentum=0.8))
-
-    # Output layer with tanh activation
-    generator.add(tf.keras.layers.Conv2DTranspose(1, (5, 5), activation='tanh', padding='same'))
-
-    return generator"""
-
-"""if __name__ == "__main__":
-    input_shape = (100,)  # Input shape for generator
-
-    # Build and compile the generator
-    generator = build_generator(input_shape)
-    generator.compile(loss='binary_crossentropy', optimizer=tf.keras.optimizers.Adam(0.0002, 0.5))
-
-    # Print the generator summary
-    generator.summary()
-
-    # Print information about the generator
-    print("Generator Architecture:")
-    generator.summary(print_fn=lambda x: print(x))
-
-    # Verify the generator's input shape
-    print(f"Generator Input Shape: {generator.input_shape}")
-"""
+model = generator()
